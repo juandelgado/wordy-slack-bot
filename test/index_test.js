4c749085cb58f76f86e6a86610fb356197f226c6
Juan Delgado <juan@ustwo.com>
2016-10-18 16:40:38 +0100

    Basic scaffolding for running tests with Mocha.

---
+++ test/index_test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+
+describe("Action / Reaction", function(){
+
+  it('Should return no action if no offending pattern found', function(){
+    assert.equal(1,1);
+  });
+});

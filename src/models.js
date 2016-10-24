fe2caa314e94b08e911a0c3b051e89ec970081a6
Juan Delgado <juan@ustwo.com>
2016-10-24 14:56:39 +0100

    Linting for #1.

--- src/models.js
+++ src/models.js
@@ -1,39 +1,33 @@
-const reactions = require('./reactions.js');
-
-function UserMessage(user_id, text){
-  this.user_id = user_id;
+function UserMessage(userId, text) {
+  this.userId = userId;
   this.text = text;
 }
 
 UserMessage.prototype.equals = function (message) {
-  return this.user_id == message.user_id &&
-    this.text == message.text;
+  return this.userId === message.userId &&
+    this.text === message.text;
 };
 
-function NoMessage(){
+function NoMessage() {
 
 }
 
-function Rule(expression, reaction){
+function Rule(expression, reaction) {
   this.expression = expression;
   this.reaction = reaction;
 }
 
-Rule.prototype.toString = function(){
-  return 'Rule [' + this.expression + ', ' + this.reaction + ']'
+function Rules() {
+  this.rules = [];
 }
 
-function Rules(){
-  this.rules = new Array();
-}
-
-Rules.prototype.add = function(rule){
+Rules.prototype.add = function (rule) {
   this.rules.push(rule);
-}
+};
 
 module.exports = {
   UserMessage,
   NoMessage,
   Rules,
-  Rule
+  Rule,
 };

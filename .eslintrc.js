f6aab84ab6ff68e623957d12651fff3b9d4ad0ee
Juan Delgado <juan@ustwo.com>
2016-10-24 17:07:48 +0100

    Linting for #1, now allowing calls to console, since this is a Node app.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: 'airbnb-base',
+  rules: {
+    // Turning off errors about using console, since this is a Node app,
+    // as recommended here: http://eslint.org/docs/rules/no-console#when-not-to-use-it
+    'no-console': 'off',
+  },
 };

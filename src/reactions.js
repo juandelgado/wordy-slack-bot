e862a13c8789ab68964202ea4754401e99c470ad
Juan Delgado <juan@ustwo.com>
2016-10-24 14:58:25 +0100

    Linting for #1.

--- src/reactions.js
+++ src/reactions.js
@@ -1,23 +1,22 @@
-function ReactionNone(){
+function ReactionNone() {
 }
 
-function ReactionDirectMessage(message){
+function ReactionDirectMessage(message) {
   this.message = message;
 }
 
 // YES, this needs the user_name, sadly not the ID as per:
 // https://github.com/mishk0/slack-bot-api
-ReactionDirectMessage.prototype.execute = function(user_name, slackBot){
-
-  const bot_message_params = {
+ReactionDirectMessage.prototype.execute = function (userName, slackBot) {
+  const botMessageParams = {
     as_user: false,
-    username: 'Wordy Bot'
-  }
+    username: 'Wordy Bot',
+  };
 
-  slackBot.postMessageToUser(user_name, this.message, bot_message_params);
-}
+  slackBot.postMessageToUser(userName, this.message, botMessageParams);
+};
 
 module.exports = {
   ReactionNone,
-  ReactionDirectMessage
+  ReactionDirectMessage,
 };

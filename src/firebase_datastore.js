17ec53a1fa2cb1cee7361208c0711a2b6c5ca338
Juan Delgado <juan@ustwo.com>
2016-11-09 18:58:45 +0000

    Bit of defensive coding just in case.

--- src/firebase_datastore.js
+++ src/firebase_datastore.js
@@ -10,12 +10,17 @@ class FirebaseDataStore {
 
   isUserInterested(userId, successCallback, errorCallback) {
     this.db.ref(`users/${userId}`).once('value', function(data) {
-      const userData = data.val();
-      const interested = (userData != null)? userData.interested : false;
-      successCallback(interested);
+
+      if (successCallback){
+        const userData = data.val();
+        const interested = (userData != null)? userData.interested : false;
+        successCallback(interested);
+      }
     }, function(errorObject) {
       console.log("Firebase DataStore error: " + errorObject);
-      errorCallback(errorObject.code);
+      if (errorCallback) {
+        errorCallback(errorObject.code);
+      }
     });
   }
 

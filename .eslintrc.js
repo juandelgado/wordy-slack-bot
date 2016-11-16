272663e414e951584b65fbcbbe957a865d0a0d7a
Arnau Siches <arnau@ustwo.com>
2016-11-16 14:05:28 +0000

    Set ESLint to allow global strict directives.
    The airbnb rules expect es6 modules but with node we don't use them. This
    change sets the `sourceType` to "script", and enforces to have a directive
    "strict" in any global scope (i.e. file).
    
    More info: http://eslint.org/docs/rules/strict
    
    Signed-off-by: Arnau Siches <arnau@ustwo.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
+'use strict';
+
 module.exports = {
+  root: true,
   extends: 'airbnb-base',
   rules: {
     // Turning off errors about using console, since this is a Node app,
     // as recommended here: http://eslint.org/docs/rules/no-console#when-not-to-use-it
     'no-console': 'off',
+    strict: ['error', 'global'],
+  },
+  parserOptions: {
+    sourceType: 'script',
+  },
+  ecmaFeatures: {
+    impliedStrict: false,
   },
 };

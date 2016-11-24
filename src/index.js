5c0327317e656edaeb7f2014ffce405387d4eac7
Juan Delgado <juan@ustwo.com>
2016-11-24 17:46:53 +0000

    Now pulling the relevant ENV vars with style.
    Shout out to @arnau!

--- src/index.js
+++ src/index.js
@@ -8,20 +8,35 @@ const servers = require('./wordy_webserver.js');
 const rules = require('./rules.js');
 const dataStore = require('./firebase_datastore.js');
 
+// Pull relevant ENV vars into constants
+const { FIREBASE_PROJECT_ID,
+        FIREBASE_CLIENT_EMAIL,
+        FIREBASE_PRIVATE_KEY,
+        FIREBASE_DB_URL,
+        SLACK_TOKEN,
+        SLACK_COMMAND_TOKEN,
+        SLACK_TEAM_ID,
+        WORDY_WEBSERVER_PORT,
+        WORDY_WEBSERVER_HOST,
+        PORT,
+        } = process.env;
+
+console.log(FIREBASE_PROJECT_ID);
+
 firebase.initializeApp({
   credential: firebase.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/(\\n)/g, '\n'),
+    projectId: FIREBASE_PROJECT_ID,
+    clientEmail: FIREBASE_CLIENT_EMAIL,
+    privateKey: FIREBASE_PRIVATE_KEY.replace(/(\\n)/g, '\n'),
   }),
-  databaseURL: process.env.FIREBASE_DB_URL,
+  databaseURL: FIREBASE_DB_URL,
 });
 
 const ds = new dataStore.FirebaseDataStore(firebase.database());
 
 // TODO: error out if no token found
 const slackBot = new SlackBot({
-  token: process.env.SLACK_TOKEN,
+  token: SLACK_TOKEN,
 });
 
 const rulesJson = JSON.parse(fs.readFileSync('./config.json', { encoding: 'utf8' }));
@@ -32,9 +47,9 @@ const wordyRules = rules.rulesFromJson(rulesJson);
 // and and instance here for consistency with the rest of the code.
 new bots.WordyBot(ds, slackBot, wordyRules);
 
-const webServerPort = process.env.PORT || process.env.WORDY_WEBSERVER_PORT || 33333;
-const webServerHost = process.env.WORDY_WEBSERVER_HOST || '0.0.0.0';
-const slackCommandToken = process.env.SLACK_COMMAND_TOKEN;
-const slackTeamId = process.env.SLACK_TEAM_ID;
+const webServerPort = PORT || WORDY_WEBSERVER_PORT || 33333;
+const webServerHost = WORDY_WEBSERVER_HOST || '0.0.0.0';
+const slackCommandToken = SLACK_COMMAND_TOKEN;
+const slackTeamId = SLACK_TEAM_ID;
 
 new servers.WordyWebServer(ds, webServerPort, webServerHost, slackCommandToken, slackTeamId);
